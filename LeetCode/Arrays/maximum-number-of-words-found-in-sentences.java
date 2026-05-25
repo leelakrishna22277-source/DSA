94830795483677f4b98009d7b9a756c6669f1261
leelakrishna22277-source <leelakrishna22277@gmail.com>
2026-05-25 11:00:34 +0530

    Create maximum-number-of-words-found-in-sentences.java

---
+++ LeetCode/Arrays/maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        
+        int count=0;
+        for(int i=0;i<sentences.length;i++){
+            String s=sentences[i];
+            int temp=1;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(j)==' '){
+                    temp+=1;
+                }
+            }
+            count=Math.max(count,temp);
+
+        }
+            return count;
+    }
+}
