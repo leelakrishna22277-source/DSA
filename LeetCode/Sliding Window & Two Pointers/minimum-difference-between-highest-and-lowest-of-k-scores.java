60bda2d5702780ec9290b699355025fbc10a23ba
leelakrishna22277-source <leelakrishna22277@gmail.com>
2026-05-25 10:20:25 +0530

    Update minimum-difference-between-highest-and-lowest-of-k-scores.java

--- LeetCode/Sliding Window & Two Pointers/minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ LeetCode/Sliding Window & Two Pointers/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -25,3 +25,20 @@ class Solution {
        return dif;
     }
 }
+------------------------------------------------------------------------------------------
+
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+       int n=nums.length;
+       int dif=Integer.MAX_VALUE;
+       Arrays.sort(nums);
+      
+       for(int i=0;i<n-k+1;i++){
+           int j=i+k-1;
+           int curdif=nums[j]-nums[i];
+           dif=Math.min(curdif,dif);
+       }
+       
+       return dif;
+    }
+}    
