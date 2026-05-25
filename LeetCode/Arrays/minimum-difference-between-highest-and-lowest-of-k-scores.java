2506d1f35e353c121a35873112d811a82fb98e08
leelakrishna22277-source <leelakrishna22277@gmail.com>
2026-05-25 10:55:52 +0530

    Update minimum-difference-between-highest-and-lowest-of-k-scores.java

--- LeetCode/Arrays/minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ LeetCode/Arrays/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -15,3 +15,41 @@ class Solution {
        return dif;
     }
 }
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        int[] arr = {9,4,1,7};
+        int sl = 2;
+        int n = arr.length;
+
+        int dif = Integer.MAX_VALUE;
+
+        Arrays.sort(arr);
+
+        int l = 0;
+
+        for(int r = 0; r < n; r++) {
+
+            al.add(arr[r]);
+
+            if(r - l + 1 > sl) {
+                al.remove(Integer.valueOf(arr[l]));
+                l++;
+            }
+
+            if(al.size() == sl) {
+                int max = Collections.max(al);
+                int min = Collections.min(al);
+
+                dif = Math.min(dif, max - min);
+            }
+        }
+
+        System.out.println(dif);
+    }
+}
