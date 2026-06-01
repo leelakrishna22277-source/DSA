a23e3632ce5bbc57f4cb8c8b84ef155f0d154eb8
leelakrishna22277-source <leelakrishna22277@gmail.com>
2026-06-01 17:50:27 +0530

    Create koko-eating-bananas.java

---
+++ LeetCode/Binary Search/koko-eating-bananas.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution {
+    public boolean isValidEatingSpeed(int[] piles,int h,int k){
+        int n=piles.length;
+        int temp=0;
+        for(int i=0;i<n;i++){
+            temp=piles[i]/k;
+            if(piles[i]%k!=0){
+                temp+=1;
+            }
+            h-=temp;
+            if(h<0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int n=piles.length;
+        int l=1;
+        int r=1000000000;
+    
+        while(l<=r){
+        int mid=l+(r-l)/2;
+        
+            if(isValidEatingSpeed(piles,h,mid)==true){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        
+       }
+        return l;
+    }
+}
