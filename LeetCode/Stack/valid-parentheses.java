3b9c6c01b9971cc848480e6776f099b7c28fd44a
leelakrishna22277-source <leelakrishna22277@gmail.com>
2026-06-08 21:43:08 +0530

    Create valid-parentheses.java

---
+++ LeetCode/Stack/valid-parentheses.java
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+
+class Solution {
+    public boolean isValid(String s) {
+        // StringBuilder sb=new StringBuilder(s);
+        // boolean b=true;
+        // // String s=;
+        // for(int i=0;i<sb.length()-1;i++){
+        //     if((sb.charAt(i)=='(' && sb.charAt(i+1)==')')){
+        //         sb.deleteCharAt(i);
+        //         sb.deleteCharAt(i);
+        //         i=-1;
+
+                
+        //     }
+        //     else if(sb.charAt(i)=='[' && sb.charAt(i+1)==']'){
+        //         sb.deleteCharAt(i);
+        //         sb.deleteCharAt(i);
+        //                  i=-1;
+
+        //     }
+        //     else if(sb.charAt(i)=='{' && sb.charAt(i+1)=='}'){
+        //         sb.deleteCharAt(i);
+        //         sb.deleteCharAt(i);
+        //                 i=-1;
+
+        //     }
+        
+        // }
+        // if(sb.length()==0){
+        //     b=true;
+        // }
+        // else{
+        //     b=false;
+        // }
+        // return b;
+
+
+        //--------------------------------------------------------------
+
+        Stack<Character> st=new Stack<>();
+      boolean isValid=true;
+      for(int i=0;i<s.length();i++){
+          if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+              st.push(s.charAt(i));
+          }
+          else if(s.charAt(i)==')'){
+              if(st.isEmpty()==true || st.peek()!='('){
+                  isValid=false;
+                  break;
+              }
+              st.pop();
+          }
+          else if(s.charAt(i)=='}'){
+              if(st.isEmpty()==true || st.peek()!='{'){
+                  isValid=false;
+                  break;
+              }
+              st.pop();
+          }
+          else if(s.charAt(i)==']'){
+              if(st.isEmpty()==true || st.peek()!='['){
+                  isValid=false;
+                  break;
+              }
+              st.pop();
+          }
+     
+      }
+       if(!st.isEmpty()){
+
+         isValid=false;
+       }
+      return isValid;
+    }
+}
